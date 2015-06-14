ab3db5d419fbefeb2146b2611c4069240c3a5996
annabelle8 <annabelle.thiebaut@gmail.com>
2015-06-14 18:17:30 +0200

    Update Auth.php

--- src/Staq/Util/Auth/Stack/Controller/Auth.php
+++ src/Staq/Util/Auth/Stack/Controller/Auth.php
@@ -17,7 +17,7 @@ class Auth extends Auth\__Parent
     const MSG_INSCRIPTION_KO = 'This username is not free.';
     const MSG_LOGIN_VALID = 'You are now connected as %s.';
     const MSG_LOGIN_KO = 'Wrong credentials.';
-    const MSG_LOGOUT_VALID = 'You are now deconnected.';
+    const MSG_LOGOUT_VALID = 'You are now disconnected.';
 
 
     /* ATTRIBUTES
@@ -117,6 +117,7 @@ class Auth extends Auth\__Parent
         }
         return \Staq::App()->getCurrentUri();
     }
+    
 
 
     /* UTIL METHODS
